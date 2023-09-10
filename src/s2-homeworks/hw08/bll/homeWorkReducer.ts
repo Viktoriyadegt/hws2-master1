d2e1a54fb42dbed976934acaaa0156f61a6c5fb3
Viktoriyadegt <vika.degtyareva.12@mail.ru>
2023-09-10 22:28:43 +0300

    HW08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,18 +7,16 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            let sortedState = [...state]
-            sortedState =  action.payload !== 'up'
-                ? state.sort(
-                    (p1, p2) => (p1.name < p2.name) ? 1 : (p1.name > p2.name) ? -1 : 0)
-                : state.sort(
-                    (p1, p2) => (p1.name > p2.name) ? 1 : (p1.name < p2.name) ? -1 : 0)
-
-            return sortedState// need to fix
+            if (action.payload === 'up') {
+                return [...state.sort((a, b) => a.name <= b.name ? -1 : 1)]
+            } else if (action.payload === 'down') {
+                return [...state.sort((a, b) => b.name <= a.name ? -1 : 1)]
+            }
+            return state
         }
         case 'check': {
 
-            return state.filter(f=>f.age>=action.payload) // need to fix
+            return state.filter(f => f.age >= action.payload) // need to fix
         }
         default:
             return state
